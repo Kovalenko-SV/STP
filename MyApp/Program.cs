ac527fea96db4470c3df7ce717d3742455228160
Kovalenko-SV <Kovalenko.Sta.V@nmu.one>
2025-09-09 19:21:38 +0300

    Add ShowGreeting function to Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine($"Hello, Word!");
+ShowGreeting();
+
+
+static void ShowGreeting()
+{
+    try
+    {
+        string userName = Environment.UserName;
+        Console.WriteLine($"Hello, {userName}!");
+    }
+    catch
+    {
+        Console.WriteLine("Hello, User!");
+    }
+}
