07063ecaffd05c4228b45abbc4395131cebdde4c
Kovalenko-SV <Kovalenko.Sta.V@nmu.one>
2025-09-09 22:30:18 +0300

    Add time display and enhanced greeting functionality

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 Console.WriteLine($"Hello, Word!");
 ShowGreeting();
-
+ShowCurrentTime(); 
 
 static void ShowGreeting()
 {
@@ -9,9 +9,17 @@ static void ShowGreeting()
     {
         string userName = Environment.UserName;
         Console.WriteLine($"Hi, {userName}!");
+        Console.WriteLine("Welcome to our application!"); 
     }
     catch
     {
         Console.WriteLine("Hi, User!");
+        Console.WriteLine("Welcome to our application!");
     }
+}
+
+
+static void ShowCurrentTime()
+{
+    Console.WriteLine($"Current time: {DateTime.Now:HH:mm:ss}");
 }
